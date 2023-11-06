1291ea02b7e217617ffaa27b21f105a9ff491cbb
Mark Adamik <m.adamik@vu.nl>
2023-11-06 08:47:34 +0100

    ...

---
+++ controllers/bvh_animation/build/release/bvh_animation.d
@@ -0,0 +1,8 @@
+build/release/bvh_animation.o: bvh_animation.c \
+ ../../libraries/bvh_util/include/webots/bvh_util.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/skin.h \
+ /usr/local/webots/include/controller/c/webots/supervisor.h \
+ /usr/local/webots/include/controller/c/webots/contact_point.h
