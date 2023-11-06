abe0f7cd2baf9c6cd83ec1b3ca238dcfa29de379
Dorteel <m.adamik@vu.nl>
2023-11-06 13:02:09 +0100

    fix bug

--- controllers/gaze_controller/gaze_controller.py
+++ controllers/gaze_controller/gaze_controller.py
@@ -2,17 +2,23 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Motor
+from controller import Robot, Motor, Supervisor, Node
 import math
+
+# create the Robot instance.
+robot = Supervisor()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
 
 YAW_ANGLE = 20
 PITCH_ANGLE = 30
 
-# create the Robot instance.
-robot = Robot()
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+cone = supervisor.getFromDef("GAZE_TRACKER")
+cone.enableContactPointsTracking(timestep)
+
+
 
 p_motor = robot.getDevice('pitch_gaze')
 y_motor = robot.getDevice('yaw_gaze')
