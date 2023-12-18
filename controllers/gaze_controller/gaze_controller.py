cb70f43298d2b99792ef20812a497b048388ff03
Mark Adamik <m.adamik@vu.nl>
2023-12-18 15:12:59 +0100

    added indexedfaceset

--- controllers/gaze_controller/gaze_controller.py
+++ controllers/gaze_controller/gaze_controller.py
@@ -6,24 +6,24 @@ from controller import Robot, Motor, Supervisor, Node
 import math
 
 # create the Robot instance.
-robot = Supervisor()
+supervisor = Supervisor()
 
 # get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+timestep = int(supervisor.getBasicTimeStep())
 
 YAW_ANGLE = 20
 PITCH_ANGLE = 30
 
 
-cone = supervisor.getFromDef("GAZE_TRACKER")
+cone = supervisor.getFromDef("gaze_tracker")
 cone.enableContactPointsTracking(timestep)
 
 
 
-p_motor = robot.getDevice('pitch_gaze')
-y_motor = robot.getDevice('yaw_gaze')
+p_motor = supervisor.getDevice('pitch_gaze')
+y_motor = supervisor.getDevice('yaw_gaze')
 
-while robot.step(timestep) != -1:
+while supervisor.step(timestep) != -1:
 
     p_motor.setPosition(math.radians(PITCH_ANGLE))
     y_motor.setPosition(math.radians(YAW_ANGLE))
